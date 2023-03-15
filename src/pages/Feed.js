76dad833e35d4a45a7ab5be939ef37c51193cb29
Abel Rodriguez <98896581+AbelRodriguezGuillen@users.noreply.github.com>
2023-03-15 00:22:49 -0400

    Update Feed.js
    delete method works, added page reload.

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -15,59 +15,73 @@ import { useNavigate } from "react-router-dom";
 
 const Feed = () => {
   const [query, setQuery] = useState("");
-  const [post, setPost] = useState([]);
-  const [id, setId] = useState("");
+  const [profile, setProfile] = useState([]);
+  // const [allData, setAllData] = useState([]);
+  const [reactId, setReactId] = useState();
 
   const navigate = useNavigate();
   const [form, setForm] = useState();
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchProfiles = async () => {
       const response = await axios.get(`http://localhost:8080/posts/${query}`);
-      setPost(response.data);
+      setProfile(response.data);
     };
-    const fetchInitialPosts = async () => {
+    const fetchInitialProfiles = async () => {
       const response = await axios.get(`http://localhost:8080/allPosts`);
       console.log(response);
-      setPost(response.data);
+      setProfile(response.data);
     };
-    if (query.length === 0) fetchInitialPosts();
-    if (query.length > 2) fetchPosts();
+    if (query.length === 0) fetchInitialProfiles();
+    if (query.length > 2) fetchProfiles();
   }, [query]);
-  console.log(post);
+  console.log(profile);
 
-  const getId = () => {
-    setId = fetch("http://localhost:8080/post/{id}", {
-      method: "GET",
-    })
-      .then((response) => console.log(response))
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const response = await axios.get(`http://localhost:8080/post/${reactId}`);
+      setReactId(response.data);
+    };
+    const deleteProfile = async () => {
+      const response = await axios.delete(
+        `http://localhost:8080/post/${reactId}`
+      );
+      console.log(response);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = getId;
-    const url = `http://localhost:8080/post/${id}`;
-    fetch(url, {
-      method: "DELETE", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => console.log(response))
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-    navigate("/employee/feed");
-  };
+    if (reactId !== null && reactId !== undefined) {
+      deleteProfile();
+      window.location.reload();
+    } else {
+      fetchProfile();
+    }
+  }, [reactId]);
+  console.log(reactId);
+
+  // const handleSubmit = (e) => {
+  //   const fetchAlldata = async () => {
+  //     const response = await axios.get(`http://localhost:8080/allPosts`);
+  //     console.log(response);
+  //     setAllData(response.data);
+  //     console.log(allData);
+  //     allData.map((p) => {
+  //       // console.log(p.id);
+  //       const deleteById = async () => {
+  //         const response = await axios.get(
+  //           `http://localhost:8080/posts` + p.id
+  //         );
+  //         console.log(response);
+  //         setId(response.data);
+  //         console.log(id);
+  //       };
+  //       // deleteById();
+  //     });
+  //     console.log(allData);
+  //   };
+  //   fetchAlldata();
+  //   console.log(fetchAlldata());
+  //   navigate("/employee/feed");
+  // };
 
   return (
     <Grid container spacing={2} sx={{ margin: "2%" }}>
@@ -91,8 +105,8 @@ const Feed = () => {
           />
         </Box>
       </Grid>
-      {post &&
-        post.map((p) => {
+      {profile &&
+        profile.map((p) => {
           return (
             <Grid key={p.id} item xs={12} md={6} lg={4}>
               <Card sx={{ padding: "3%", overflow: "hidden", width: "84%" }}>
@@ -124,8 +138,7 @@ const Feed = () => {
                     </Typography>
                   );
                 })}
-                {/* Delete btn */}
-                <form autoComplete="off" noValidate onSubmit={handleSubmit}>
+                <Typography>
                   <Box
                     sx={{
                       display: "flex",
@@ -137,12 +150,13 @@ const Feed = () => {
                       sx={{ width: "10%", margin: "2% auto" }}
                       variant="contained"
                       type="submit"
-                      // onSubmit={handleButtonClick}
+                      value={p.id}
+                      onClick={(e) => setReactId(e.target.value)}
                     >
                       Delete
                     </Button>
                   </Box>
-                </form>
+                </Typography>
               </Card>
             </Grid>
           );
