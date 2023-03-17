5acfc5f7e4ae575c84414f0e47bbf39bd353c4ec
Abel Rodriguez <98896581+AbelRodriguezGuillen@users.noreply.github.com>
2023-03-17 00:44:46 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,20 +13,36 @@ const Home = () => {
         <ul className="ul">
           <li>
             <Button
-              sx={{ margin: "2% 3%" }}
-              variant="outlined"
+              sx={{ margin: "2% 3%", marginTop: "200px", color: "white" }}
+              variant="contained"
               className="home-btn"
             >
-              <Link to="/employer/create">Create a Profile</Link>
+              <Link
+                to="/api/post/create"
+                style={{
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+              >
+                Create a Profile
+              </Link>
             </Button>
           </li>
           <li>
             <Button
-              sx={{ margin: "2% 3%" }}
-              variant="outlined"
+              sx={{ margin: "2% 3%", color: "white" }}
+              variant="contained"
               className="home-btn"
             >
-              <Link to="/employee/feed">View Profiles</Link>
+              <Link
+                to="/api/post/feed"
+                style={{
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+              >
+                View Profiles
+              </Link>
             </Button>
           </li>
         </ul>
@@ -36,40 +52,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// import React from "react";
-// // import { Typography, Button } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import "../App.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// const Home = () => {
-//   return (
-//     <div>
-//       <h1
-//         sx={{ margin: "5%" }}
-//         variant="h3"
-//         align="center"
-//         className="container bg-white p-4 mt-5"
-//       >
-//         Profile Search & Creator
-//       </h1>
-//       <div>
-//         <ul className="ul">
-//           <li>
-//             <button type="button" class="btn btn-light">
-//               <Link to="/employer/dashboard">Create a Profile</Link>
-//             </button>
-//           </li>
-//           <li>
-//             <button type="button" class="btn btn-light">
-//               <Link to="/employee/feed">View Profiles</Link>
-//             </button>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
